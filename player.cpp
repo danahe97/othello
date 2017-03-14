702820284b234c57822b123975a800d6854b1b98
danahe97 <dhhe@caltech.edu>
2017-03-14 14:52:03 -0700

    minimax improvements

--- player.cpp
+++ player.cpp
@@ -45,6 +45,96 @@ Player::~Player() {
  * The move returned must be legal; if there are no valid moves for your side,
  * return nullptr.
  */
+vector <Move*> Player::makepossible(Side side, Board* board)
+{
+	vector <Move*> possible;
+	for (int i = 0; i < 8; i++)
+	{ 
+		for (int j = 0; j < 8; j++)
+		{
+			Move* move = new Move(i, j);
+			if (board->checkMove(move, side))
+			{
+				possible.push_back(move);
+			}
+			else
+				delete move;
+		}
+	}
+	return possible;
+}
+int findscore(Move* possible)
+{
+	int score = 0;
+	// If corner, add one, if next to corner, subtract one
+	if (possible->x == 0)
+	{
+		if (possible->y == 0 || possible->y == 7)
+			score += 1;
+		if (possible->y == 1 || possible->y == 6)
+			score -= 1;
+	}
+	if (possible->x == 7)
+	{
+		if (possible->y == 0 || possible->y == 7)
+			score += 1;
+		if (possible->y == 1 || possible->y == 6)
+			score -= 1;
+	}
+	if (possible->x == 1)
+	{
+		if (possible->y == 0 || possible->y == 7)
+			score -= 1;
+		if (possible->y == 1 || possible->y == 6)
+			score -= 1;
+	}	
+	if (possible->x == 6)
+	{
+		if (possible->y == 0 || possible->y == 7)
+			score -= 1;
+		if (possible->y == 1 || possible->y == 6)
+			score -= 1;
+	}
+	return score;
+}
+Move* bestmove(vector <Move*> possible)
+{
+	int maxscore = 0;
+	int score = 0;
+	Move* themove;
+	for (unsigned int i = 0; i < possible.size(); i++)
+	{
+		score = findscore(possible[i]);
+		// Continuously find best move		
+		if (score > maxscore || i == 0)
+		{
+			maxscore = score;
+			themove = possible[i];
+		}
+	}
+	return themove;
+}
+Move* Player::minimove(vector<Move*>possible, Board* board)
+{
+	int lowscore = 0;
+	Move* themove;
+	for (unsigned int i = 0; i < possible.size(); i++)
+	{
+		Board *copy = board->copy();
+		copy->doMove(possible[i], myside);
+		vector <Move*> oppossible = makepossible(otherside, copy);
+		if (oppossible.size() == 0)
+			return possible[i];
+		int score = findscore(bestmove(oppossible));
+		if (score < lowscore || i == 0)
+		{
+			lowscore = score;
+			themove = possible[i];
+		}
+		delete copy;
+	}
+	return themove;
+}
 Move *Player::doMove(Move *opponentsMove, int msLeft) {
     /*
      * TODO: Implement how moves your AI should play here. You should first
@@ -54,82 +144,18 @@ Move *Player::doMove(Move *opponentsMove, int msLeft) {
 	// process opponenet's move
 	board->doMove(opponentsMove, otherside);
 
-	// create vector of all possible moves
-	vector <Move*> possible;
-	for (int i = 0; i < 8; i++)
-	{ 
-		for (int j = 0; j < 8; j++)
-		{
-			Move* move = new Move(i, j);
-			if (board->checkMove(move, myside))
-			{
-				possible.push_back(move);
-			}
-		}
-	}
-
 	if (!board->hasMoves(myside))
-	{
 		return nullptr;
-	}
+	//Find possible moves for us
+	vector <Move*> possible = makepossible(myside, board);
+	Move* mymove;
+	mymove = minimove(possible, board);
 
 	// heuristic function
-	int maxscore = 0;
-	Move* themove;
-	for (unsigned int i = 0; i < possible.size(); i++)
-	{
-		int score = 0;
-		if (possible[i]->x == 0)
-		{
-			if (possible[i]->y == 0 || possible[i]->y == 7)
-			{
-				score += 1;
-			}
-			if (possible[i]->y == 1 || possible[i]->y == 6)
-			{
-				score -= 1;
-			}
-		}
-		if (possible[i]->x == 7)
-		{
-			if (possible[i]->y == 0 || possible[i]->y == 7)
-			{
-				score += 1;
-			}
-			if (possible[i]->y == 1 || possible[i]->y == 6)
-			{
-				score -= 1;
-			}
-		}
-		if (possible[i]->x == 1)
-		{
-			if (possible[i]->y == 0 || possible[i]->y == 7)
-			{
-				score -= 1;
-			}
-			if (possible[i]->y == 1 || possible[i]->y == 6)
-			{
-				score -= 1;
-			}
-		}	
-		if (possible[i]->x == 6)
-		{
-			if (possible[i]->y == 0 || possible[i]->y == 7)
-			{
-				score -= 1;
-			}
-			if (possible[i]->y == 1 || possible[i]->y == 6)
-			{
-				score -= 1;
-			}
-		}		
-		if (score > maxscore || i == 0)
-		{
-			maxscore = score;
-			themove = possible[i];
-		}
-	}
-
-	board->doMove(themove, myside);
-	return themove;
+//	if (testingMinimax == false)
+//		themove = bestmove(possible);
+//	else
+//		themove = minimove(possible);
+	board->doMove(mymove, myside);
+	return mymove;
 }
--- player.hpp
+++ player.hpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include "common.hpp"
 #include "board.hpp"
+#include <vector>
 using namespace std;
 
 class Player {
@@ -14,7 +15,9 @@ public:
     Side myside;
     Side otherside;
     ~Player();
+   	vector <Move*> makepossible(Side side, Board* board);
 
+	Move *minimove(vector<Move*>possible, Board* board);
     Move *doMove(Move *opponentsMove, int msLeft);
 
     // Flag to tell if the player is running within the test_minimax context
